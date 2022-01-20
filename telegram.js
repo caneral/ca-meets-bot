7ecb57f9470e469ae5ef8441cb344b24b2467617
Caner <38960258+caneral@users.noreply.github.com>
2022-01-20 21:41:35 +0300

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -3,37 +3,23 @@ const token = process.env.TOKEN;
 const bot = new telegramBot(token, { polling: true });
 const chatId = process.env.CHATID;
 
-const meetLink = "meet.google.com/atf-vftc-ftd"
+const meetLink = "meet.google.com/atf-vftc-ftd";
 
 // 1340956785
-var date;
-var hour;
-var minute;
+var currentTime = new Date().getTime();
+var meetTime = 1642704300;
+
+console.log(currentTime);
 var isOkay = 0;
-var developers = ["Aykut Karakuş", "Caner Al", "Furkan Yıldız","Hilal Karaman", "Kübra Göktaş", "Merve Kocabay", "Mustafa Şahin", "Nur Kifayet Önal", "Okan Kozanhan", "Ömer Furkan Doğruyol", "Şerife Yaman", "Yaren Bıldırcın"]; 
-var devLength = developers.length;
-var queue = 0;
 
-function dateTime() {
-  date = new Date();
-  hour = date.getHours();
-  minute = date.getMinutes();  
-    // bot.sendMessage(chatId,hour+":"+minute)
+function meetNotification() {
+      bot.sendMessage(
+        chatId,
+        "Saat 21.00'de toplantı olacaktır. Tüm üyelerin katılması beklenmektedir.\n\n Toplantı Adresi: " +
+          meetLink
+      );
+  } 
 
-  if (hour === 20 && minute === 59) {
-    // bot.sendMessage(chatId,hour+":"+minute)
-    if (isOkay === 0) {
-      bot.sendMessage(chatId, "Saat 21.00'de toplantı olacaktır. Tüm üyelerin katılması beklenmektedir.\n\n Toplantı Adresi: " + meetLink);
-      
-      queue += 1;
-      if(queue === devLength){
-          queue = 0;
-      }
-      isOkay = 1;
-    }
-  }else {    
-      isOkay = 0;
-  }
-}
-
-setInterval(dateTime, 1000);
+setTimeout(function () {
+  meetNotification()
+}, meetTime - currentTime);
